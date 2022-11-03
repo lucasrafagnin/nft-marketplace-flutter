b4fa2f0dd9b3ae4365e5c52166901278519fe967
Lucas Rafagnin <lucasrafagnin7@gmail.com>
2022-11-02 23:47:45 -0300

    Improve nft detail, show back button

--- lib/features/collection/widgets/cell_collection_item.dart
+++ lib/features/collection/widgets/cell_collection_item.dart
@@ -9,77 +9,77 @@ class CellCollectionItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: _collection.contract,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 16),
-        child: InkResponse(
-          onTap: () => Navigator.pushNamed(
-            context,
-            CollectionDetail.routeName,
-            arguments: _collection,
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              CircleAvatar(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 16),
+      child: InkResponse(
+        onTap: () => Navigator.pushNamed(
+          context,
+          CollectionDetail.routeName,
+          arguments: _collection,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Hero(
+              tag: _collection.contract,
+              child: CircleAvatar(
                 radius: 24,
                 backgroundImage: NetworkImage(_collection.logo),
               ),
-              const SizedBox(width: 12),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      _collection.name,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
-                      ),
+            ),
+            const SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    _collection.name,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
                     ),
-                    Text(
-                      _collection.getCategoryName(),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        fontSize: 13,
-                      ),
+                  ),
+                  Text(
+                    _collection.getCategoryName(),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 13,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      _collection.getMarketCapFormatted(),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
-                      ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    _collection.getMarketCapFormatted(),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
                     ),
-                    Text(
-                      _collection.getMarketCapChangeAsPercentage(),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 13,
-                        color: _collection.marketCapChangePercentage < 0
-                            ? Colors.red
-                            : Colors.green,
-                      ),
+                  ),
+                  Text(
+                    _collection.getMarketCapChangeAsPercentage(),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: _collection.marketCapChangePercentage < 0
+                          ? Colors.red
+                          : Colors.green,
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/features/nft/pages/nft_detail_page.dart
+++ lib/features/nft/pages/nft_detail_page.dart
@@ -32,25 +32,89 @@ class NftDetailState extends State<NftDetail> {
   Widget build(BuildContext context) {
     var nft = widget.nft;
     return Scaffold(
-      backgroundColor: Colors.black,
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Image.network(
-                nft.image,
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.fill,
-              ),
-              const SizedBox(height: 24),
-              ValueListenableBuilder(
-                valueListenable: _controller.nft,
-                builder: (context, value, child) =>
-                    Text(value != null ? value.name : ""),
-              ),
-            ],
-          ),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image.network(
+                  nft.image,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.fitWidth,
+                ),
+                Positioned(
+                  left: 20,
+                  top: 40,
+                  child: SizedBox(
+                    height: 56,
+                    child: ElevatedButton(
+                      onPressed: () => Navigator.pop(context),
+                      style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        backgroundColor: Colors.transparent
+                      ),
+                      child: const Icon(
+                        Icons.arrow_back_rounded,
+                        size: 32,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 16,
+                  top: 40,
+                  child: SizedBox(
+                    height: 56,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        backgroundColor: const Color(0xA6FFFFFF),
+                      ),
+                      child: const Icon(
+                        Icons.favorite,
+                        size: 24,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            ValueListenableBuilder(
+              valueListenable: _controller.nft,
+              builder: (context, value, child) => _buildNftDetail(value),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildNftDetail(Nft? nft) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: const [
+          Text(
+            "Nome do NFT",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 22,
+            ),
+            textAlign: TextAlign.start,
+          ),
+          Divider(height: 40),
+          Text(
+            "Descrição do NFTDescrição do NFT Descrição do NFT Descrição do NFT Descrição do NFT Descrição do NFT Descrição do NFT",
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 13,
+            ),
+          )
+        ],
       ),
     );
   }
